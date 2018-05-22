c91d3082f1c288373f8be9f11cb44c00a42f0848
D3v3l0per1 <cybergatea41@gmail.com>
2018-05-22 18:13:27 +0200

    Initial Commit - With the Menu

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+
+    <v-toolbar>
+      <v-toolbar-side-icon class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Social_Network</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="item in menuItems" :key="item.title">{{ item.title }}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuItems: [
+        { title: 'Test1', link: '' },
+        { title: 'Test2', link: '' },
+        { title: 'Test3', link: '' }
+      ]
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#4CAF50',
+    secondary: '#A5D6A7',
+    accent: '#33691E',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#2196f3',
+    success: '#00E676'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
